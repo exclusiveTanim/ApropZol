42628a9b3ebdcea3377b48456105c4c81bbc8db3
exclusiveTanim <gsl001@Mohammads-MacBook-Pro-4.local>
2015-10-25 14:42:31 +0600

    Final Project Added

---
+++ app/controllers/index.js
@@ -0,0 +1,33 @@
+var osname = Ti.Platform.osname;
+// Booleans identifying the platforms are handy too
+var isAndroid = (osname == 'android') ? true : false;
+if (isAndroid) {
+	// do Android specific stuff
+	$.textField.backgroundColor="#DCDCDC";
+	$.textField1.backgroundColor="#DCDCDC";
+	$.textField.color="black";
+	$.textField1.color="black";
+	Ti.API.info("Platform is Android");
+} else {
+	// do iOS, mobileweb, or other platform stuff
+	Ti.API.info("Platform is iOS");
+}
+
+function doClick(e) {
+	Ti.API.info("Get Project Button was clicked");
+	var user = $.textField.value;
+	var pass = $.textField1.value;
+	//Ti.API.info(user);
+	if (user == 'a' && pass == '1') {
+		var win2 = Alloy.createController('bluewin').getView();
+		//$.win1.openWindow(win2);
+		win2.open();
+	} else {
+		alert('Insert valid user and password');
+		$.textField.value = '';
+		$.textField1.value = '';
+	}
+
+}
+
+$.win1.open();
